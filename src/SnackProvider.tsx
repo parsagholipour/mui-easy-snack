5ac817e113a690bebccee959bc1b71af76f0003c
Parsa <12parsa@gmail.com>
2024-12-02 12:05:55 +0330

    fix: createContext is not defined

--- src/SnackProvider.tsx
+++ src/SnackProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Snackbar from '@mui/material/Snackbar';
 import {SnackContext} from "./SnackContext.ts";
 import { SnackbarProps } from '@mui/material/Snackbar';
